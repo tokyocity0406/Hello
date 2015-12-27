3e20ef83cb9b7e89409579a288b86141e20ea43d
takasuga <tokyocity0406@gmail.com>
2015-12-27 19:57:13 +0900

    initail commit

---
+++ .chef/DIRECTORY/client.rb
@@ -0,0 +1,4 @@
+log_level        :info
+log_location     STDOUT
+chef_server_url  'https://USER-no-MacBook-Air-3.local:443'
+validation_client_name 'chef-validator'
---
+++ .chef/knife.rb
@@ -0,0 +1,9 @@
+log_level                :info
+log_location             STDOUT
+node_name                'user'
+client_key               '/Users/user/chef-repo/.chef/user.pem'
+validation_client_name   'chef-validator'
+validation_key           '/etc/chef-server/chef-validator.pem'
+chef_server_url          'https://USER-no-MacBook-Air-3.local:443'
+syntax_check_cache_path  '/Users/user/chef-repo/.chef/syntax_check_cache'
+cookbook_path [ '/Users/user/chef-repo/cookbooks' ]
